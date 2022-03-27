52ee8151d7a100dbd78bc3d4071aacb10b5db3aa
christopher <cdnance1829@gmail.com>
2022-03-27 17:44:55 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b){
+    return ( a + b );
+}
+
+function subtract(a, b){
+    return ( a - b );
+}
+
+function multiply(a, b){
+    return ( a * b );
+}
+
+function divide(a, b){
+    return ( a / b);
+}
+
+function increment(number){
+        number++;
+    return number;
+}
+
+function decrement(number){
+        number--;
+    return number;    
+}
+
+function makeInt(number){
+    return parseInt(number, 10);
+}
+
+function preserveDecimal(number){
+    return parseFloat(number);
+}
